fe5ef7e8fe9649c86aa6660a623ad9d2fe7baace
Tomasz Olęcki <justicar.alaric@gmail.com>
2023-11-22 14:10:31 +0100

    Update Swagger configuration in web API
    This commit refines the use of Swagger for API documentation in the web application. It introduces the OpenApi.Models namespace and modifies the Swagger generation setup. Also, some additional adjustments have been made to clean up and reorganize the code for better readability.

--- SimpleWebApi8/Program.cs
+++ SimpleWebApi8/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Marten;
 using Marten.Events.Projections;
 using SimpleWebApi8;
@@ -5,32 +6,25 @@ using Weasel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Event Sourcing API", Version = "v1" }); });
 var connectionString = builder.Configuration.GetConnectionString("Postgres");
-
 builder.Services.AddMarten(options =>
     {
         if (connectionString != null) options.Connection(connectionString);
         options.AutoCreateSchemaObjects = AutoCreate.All;
     })
     .ApplyAllDatabaseChangesOnStartup();
-
 builder.Services.AddScoped(GetDeviceStore);
-
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
 app.MapGet("/", () => "Hello World!");
-// add endpoint that returns a random number
 app.MapGet("/random", () => new Random().Next(1, 100).ToString());
-
 app.MapGet("createDevice", DeviceHandlers.CreateDevice);
-
 app.MapGet("device/{deviceId:guid}", DeviceHandlers.GetDeviceById);
-
 app.Run();
 return;
 
@@ -44,4 +38,6 @@ DocumentStore GetDeviceStore(IServiceProvider serviceProvider)
     return documentStore;
 }
 
-public partial class Program { }
+public partial class Program
+{
+}
