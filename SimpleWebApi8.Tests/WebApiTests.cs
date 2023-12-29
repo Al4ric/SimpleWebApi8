f991b77a00718d3e197f26cd9cd04ad258e3a0cb
Tomasz Olęcki <justicar.alaric@gmail.com>
2023-12-29 14:59:03 +0100

    Refactor device handling and event sourcing

--- SimpleWebApi8.Tests/WebApiTests.cs
+++ SimpleWebApi8.Tests/WebApiTests.cs
@@ -12,10 +12,10 @@ public class WebAppTest : IClassFixture<CustomWebAppFactory<Program>>
     }
 
     [Fact]
-    public async Task TestCreateDeviceEndpoint()
+    public async Task TestCreateMyThingEndpoint()
     {
         // Arrange
-        var httpRequest = new HttpRequestMessage(HttpMethod.Get, "/createDevice");
+        var httpRequest = new HttpRequestMessage(HttpMethod.Get, "/createMyThing");
 
         // Act
         var httpResponse = await _client.SendAsync(httpRequest);
@@ -23,8 +23,55 @@ public class WebAppTest : IClassFixture<CustomWebAppFactory<Program>>
         // Assert
         httpResponse.EnsureSuccessStatusCode(); // fails the test if the status code is not a success status code (2xx)
         var responseContent = await httpResponse.Content.ReadAsStringAsync();
-        var device = JsonConvert.DeserializeObject<Device>(responseContent);
+        var myThing = JsonConvert.DeserializeObject<MyThing>(responseContent);
 
-        Assert.Equal("My Device", device?.Name);
+        Assert.Equal("My Thing", myThing?.Name);
+    }
+
+    [Fact]
+    public async Task TestCreateAndRetrieveMyThingEndpoint()
+    {
+        // Arrange
+        var createRequest = new HttpRequestMessage(HttpMethod.Get, $"/createMyThing");
+
+        // Act
+        var createResponse = await _client.SendAsync(createRequest);
+
+        // Assert
+        createResponse.EnsureSuccessStatusCode(); // fails the test if the status code is not a success status code (2xx)
+        var createResponseContent = await createResponse.Content.ReadAsStringAsync();
+        var createdMyThing = JsonConvert.DeserializeObject<MyThing>(createResponseContent);
+
+        var myThingId = createdMyThing?.Id ?? Guid.Empty;
+        var myThing = new MyThing("My Thing", myThingId)
+        {
+            Description = "This is my thing",
+            Tags = ["tag1", "tag2"],
+            Picture = [], // replace with your picture bytes
+            Place = "My place"
+        };
+
+        Assert.Equal(myThing.Name, createdMyThing?.Name);
+        Assert.Equal(myThing.Description, createdMyThing?.Description);
+        Assert.Equal(myThing.Tags, createdMyThing?.Tags);
+        Assert.Equal(myThing.Picture, createdMyThing?.Picture);
+        Assert.Equal(myThing.Place, createdMyThing?.Place);
+
+        // Arrange
+        var retrieveRequest = new HttpRequestMessage(HttpMethod.Get, $"/myThing/{myThingId}");
+
+        // Act
+        var retrieveResponse = await _client.SendAsync(retrieveRequest);
+
+        // Assert
+        retrieveResponse.EnsureSuccessStatusCode(); // fails the test if the status code is not a success status code (2xx)
+        var retrieveResponseContent = await retrieveResponse.Content.ReadAsStringAsync();
+        var retrievedMyThing = JsonConvert.DeserializeObject<MyThing>(retrieveResponseContent);
+
+        Assert.Equal(myThing.Name, retrievedMyThing?.Name);
+        Assert.Equal(myThing.Description, retrievedMyThing?.Description);
+        Assert.Equal(myThing.Tags, retrievedMyThing?.Tags);
+        Assert.Equal(myThing.Picture, retrievedMyThing?.Picture);
+        Assert.Equal(myThing.Place, retrievedMyThing?.Place);
     }
 }
--- SimpleWebApi8/Program.cs
+++ SimpleWebApi8/Program.cs
@@ -23,8 +23,8 @@ app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/random", () => new Random().Next(1, 100).ToString());
-app.MapGet("createDevice", DeviceHandlers.CreateDevice);
-app.MapGet("device/{deviceId:guid}", DeviceHandlers.GetDeviceById);
+app.MapGet("createMyThing", ThingHandlers.CreateMyThing);
+app.MapGet("myThing/{myThingId:guid}", ThingHandlers.GetMyThingById);
 app.Run();
 return;
 
@@ -33,7 +33,7 @@ DocumentStore GetDeviceStore(IServiceProvider serviceProvider)
     var documentStore = DocumentStore.For(options =>
     {
         if (connectionString != null) options.Connection(connectionString);
-        options.Projections.Snapshot<Device>(SnapshotLifecycle.Inline);
+        options.Projections.Snapshot<MyThing>(SnapshotLifecycle.Inline);
     });
     return documentStore;
 }
